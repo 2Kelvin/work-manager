56e90e73370edc7af38d5e5696f19068a814410f
2Kelvin <rocketmancodes02@gmail.com>
2024-08-10 09:33:41 +0300

    chain workers

--- app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -2,10 +2,14 @@ package com.example.bluromatic.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
 import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.KEY_BLUR_LEVEL
+import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -18,6 +22,9 @@ class BlurWorker(ctx: Context, params: WorkerParameters)
 {
 
     override suspend fun doWork(): Result {
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+        val blurLevel = inputData.getInt(KEY_BLUR_LEVEL, 1)
+
         makeStatusNotification(
             applicationContext.resources.getString(R.string.blurring_image),
             applicationContext
@@ -25,22 +32,25 @@ class BlurWorker(ctx: Context, params: WorkerParameters)
 
         return withContext(Dispatchers.IO) {
             return@withContext try {
-                // emulating slower work for delay between notification messages
-                delay(DELAY_TIME_MILLIS)
+                require(!resourceUri.isNullOrBlank()) {
+                    val errorMessage = applicationContext.resources
+                        .getString(R.string.invalid_input_uri)
+                    Log.e(TAG, errorMessage)
+                    errorMessage
+                }
 
-                val picture = BitmapFactory.decodeResource(
-                    applicationContext.resources,
-                    R.drawable.android_cupcake
+                val resolver = applicationContext.contentResolver
+                delay(DELAY_TIME_MILLIS) // emulating slower work for delay between notification messages
+
+                val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri))
                 )
 
-                // blurring the image
-                val output = blurBitmap(picture, 1)
-                // writing the blurred image to a temporary file
-                val outputUri = writeBitmapToFile(applicationContext, output)
-                // displaying a message to the UI of the blurred image output file
-                makeStatusNotification("Output is $outputUri", applicationContext)
+                val output = blurBitmap(picture, blurLevel) // blurring the image
+                val outputUri = writeBitmapToFile(applicationContext, output) // writing the blurred image to a temporary file
+                val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
 
-                Result.success()
+                Result.success(outputData)
             } catch (throwable: Throwable) {
                 Log.e(
                     TAG,
